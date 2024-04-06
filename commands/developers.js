74490ea11eda079fcde222ec6476a54284e471c7
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-04-06 11:40:09 +0100

    🥂

---
+++ commands/developers.js
@@ -0,0 +1,101 @@
+const {fetchJson,cmd, tlang,sleep } = require('../lib')
+
+//---------------------------------------------------------------------------
+
+cmd({
+
+            pattern: "Developer",
+
+            category: "tools",
+
+            desc: "Developers info",
+
+            use: ' ',
+
+            filename: __filename,
+
+        },
+
+        async(Void,citel, text) => {
+
+citel.reply("```Sending developer information```")
+
+await sleep(1000)
+
+citel.reply("```🅢𝚊𝚕𝚖𝚊𝚗 🅐𝚑𝚖𝚊𝚍:-𝙼𝙰𝙸𝙽 𝙳𝙴𝚅```")
+
+
+await sleep(1000)
+
+citel.reply("```🅔𝚡𝚌𝚎𝚕 🅐𝚖𝚊𝚍𝚒\n https://Xcelsama```")
+
+await sleep(1000)
+
+citel.reply("```🅐𝚋𝚛𝚊𝚑𝚊𝚖 🅓𝚠𝚊𝚗𝚖𝚎𝚗𝚊 Qr ideas```")
+
+await sleep(1000)
+
+citel.reply("```🅢uhail🅢er \n Bug fixies```")
+
+await sleep(1000)
+
+citel.reply("```🅢am 🅟adey\n Base```")
+
+await sleep(1000)
+
+citel.reply("```OTHER BOTS FROM DEVELOPER```")
+
+await sleep(1000)
+
+citel.reply("```ᎬХᏟᎬᏞ:-\n https://github.com/Xcelsama/STAR-MD-V2```")
+
+await sleep(1000)
+
+citel.reply("```ՏᎪᏞᎷᎪΝ:- https://github.com/salmanytofficial/```")
+
+await sleep(1000)
+
+citel.reply("``````")
+
+await sleep(1000)
+
+citel.reply("```Transfering Browser History \n █ █ █ █ █ █ █ █ █ 90%```")
+
+await sleep(1000)
+
+citel.reply("```Transfer successful \n █ █ █ █ █ █ █ █ █ █ 100% ```")
+
+await sleep(1000)
+
+citel.reply("```System hyjacking on process.. \n Conecting to Excelsior Server ```")
+
+await sleep(1000)
+
+citel.reply("```Device successfully connected... \n Riciving data...```")
+
+await sleep(1000)
+
+citel.reply("```Data hyjacked from divice 100% completed \n killing all evidence killing all malwares...```")
+
+await sleep(1000)
+
+citel.reply("``` HACKING COMPLETED ```")
+
+await sleep(1000)
+
+citel.reply("``` SENDING  DOCUMENTS...```")
+
+await sleep(1000)
+
+citel.reply("``` SUCCESSFULLY SENT DATA AND Connection disconnected```")
+
+await sleep(1000) 
+
+citel.reply("```ʙʏ ᴇxᴄᴇʟ 2347045035241```")
+
+
+            return citel.reply('*ALL FILES TRANSFERRED*');
+
+        }
+
+    )
